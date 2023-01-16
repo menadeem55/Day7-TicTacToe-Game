f3d502b5227ecded3ef493eeb74eecacc66c3efa
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-16 23:35:02 +0530

    Added: Creating Board

--- src/com/blabz/tictactoe/TicTacToe.java
+++ src/com/blabz/tictactoe/TicTacToe.java
@@ -1,10 +1,18 @@
 package com.blabz.tictactoe;
 
 public class TicTacToe {
-	
-	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Tic Tac Toe");
+private static char[] board = new char[10];
+	
+	static void startGame() {
+		for (int i = 1; i <= 9; i++) {
+			board[i] = 1;
+			System.out.println(board);
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Tic Tac Toe ");
+		startGame();
 	}
 
 }
